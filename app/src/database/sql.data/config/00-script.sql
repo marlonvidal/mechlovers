c94d99bf1e417632a69ef97164d840a4f6dd4600
Marlon VidalFerreira <MVidalFerreira@fanatics.com>
2018-09-14 16:04:46 -0700

    Refatorado diretorios do BD

---
+++ app/src/database/sql.data/config/00-script.sql
@@ -0,0 +1,17 @@
+-- --------------------------------------------------------
+-- Host:                         localhost
+-- Server version:               5.6.41 - MySQL Community Server (GPL)
+-- Server OS:                    Linux
+-- HeidiSQL Version:             9.5.0.5196
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Dumping database structure for mechlovers.usuario
+CREATE DATABASE IF NOT EXISTS `mechlovers_usuarios` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `mechlovers_usuarios`;
---
+++ app/src/database/sql.data/config/01-permissions.sql
@@ -0,0 +1,2 @@
+CREATE USER 'mechlovers'@'%' IDENTIFIED BY 'mechlovers';
+GRANT ALL PRIVILEGES ON mechlovers_usuarios.* TO 'mechlovers'@'%' WITH GRANT OPTION;
